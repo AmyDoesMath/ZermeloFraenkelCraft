5c996506765af61e7a8661f02cd66293ed404bf9
AmyDoesMath <plancklength4@gmail.com>
2023-04-03 03:06:01 +0300

    some stuff

---
+++ src/main/java/io/github/amydoesmath/zfcraft/block/Chalkboard.java
@@ -0,0 +1,9 @@
+package io.github.amydoesmath.zfcraft.block;
+
+import net.minecraft.block.Block;
+
+public class Chalkboard extends Block {
+	public Chalkboard(Settings settings) {
+		super(settings);
+	}
+}
---
+++ src/main/java/io/github/amydoesmath/zfcraft/item/ChalkboardItem.java
@@ -0,0 +1,10 @@
+package io.github.amydoesmath.zfcraft.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+
+public class ChalkboardItem extends BlockItem {
+	public ChalkboardItem(Block block, Settings settings) {
+		super(block, settings);
+	}
+}
